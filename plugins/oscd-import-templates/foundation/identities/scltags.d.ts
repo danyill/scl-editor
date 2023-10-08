a205fde4c67b9338b9a32dd5ab93424f8e6fc7a8
Daniel Mulholland <dan.mulholland@gmail.com>
2023-10-09 11:03:46 +1300

    Updates

--- plugins/oscd-import-templates/foundation/identities/scltags.d.ts
+++ plugins/oscd-import-templates/foundation/identities/scltags.d.ts
@@ -1,6 +1,6 @@
 import { SCLTag } from '../utils/scldata.js';
-type IdentityFunction = (e: Element) => string | number;
-type SelectorFunction = (tagName: SCLTag, identity: string) => string;
+declare type IdentityFunction = (e: Element) => string | number;
+declare type SelectorFunction = (tagName: SCLTag, identity: string) => string;
 export declare const tags: Record<SCLTag, {
     identity: IdentityFunction;
     selector: SelectorFunction;
